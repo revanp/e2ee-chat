18b2efc4eb9d6362180beb86acde2314f81fca8b
Revan Pratama <revanp0@gmail.com>
2024-12-25 15:26:12 +0700

    [Feat] jalannnn

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { decryptMessage, encryptMessage, generateKeyPair } from "~/utils/crypto";
 import { connectWebSocket } from "~/utils/websocket";
 
@@ -12,10 +12,12 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   const [messages, setMessage] = useState<string[]>([]);
   const [input, setInput] = useState("");
-  const [socket, setSocket] = useState<WebSocket | null>(null);
+  const socket = useRef<WebSocket | null>(null);
 
   useEffect(() => {
     const setupSocket = async () => {
+      if (socket.current) return;
+
       const { publicKey, privateKey }  = await generateKeyPair();
       const sharedKey = new Uint8Array(32); // Use a consistent key for encryption and decryption
 
@@ -33,21 +35,27 @@ export default function Index() {
         }
       });
 
-      setSocket(ws);
+      socket.current = ws;
     }
 
     setupSocket();
+
+    return () => {
+      if (socket.current) {
+        socket.current.close();
+      }
+    };
   }, []);
 
   const sendMessage = async () => {
-    if(!socket) {
+    if(!socket.current) {
       return;
     }
 
     const sharedKey = new Uint8Array(32);
     const { ciphertext, nonce } = await encryptMessage(input, sharedKey);
 
-    socket.send(JSON.stringify({ ciphertext, nonce }));
+    socket.current.send(JSON.stringify({ ciphertext, nonce }));
     setInput('');
   }
 
